4db04e4bf5b426c58d3725add821f9c274a923f1
syscl <yating@deductive.ai>
2024-12-14 11:44:29 -0800

    add header

--- opensearch-cross-account-role.tf
+++ opensearch-cross-account-role.tf
@@ -1,3 +1,12 @@
+/*
+ Copyright (c) 2023, Deductive AI, Inc. All rights reserved.
+
+ This software is the confidential and proprietary information of
+ Deductive AI, Inc. You shall not disclose such confidential
+ information and shall use it only in accordance with the terms of
+ the license agreement you entered into with Deductive AI, Inc.
+*/
+
 #################################################
 # Variables
 #################################################
