ac7883b060c8f58b5382b636f414ce223876e33f
syscl <yating@deductive.ai>
2025-11-05 14:50:04 -0800

    with eks improvements

--- modules/bootstrap/data.tf
+++ modules/bootstrap/data.tf
@@ -432,7 +432,10 @@ data "aws_iam_policy_document" "deductive_policy" {
       "eks:DeregisterCluster",
       "eks:RegisterCluster",
       "eks:TagResource",
-      "eks:UntagResource"
+      "eks:UntagResource",
+      "eks:AssociateAccessPolicy",
+      "eks:DisassociateAccessPolicy",
+      "eks:ListAssociatedAccessPolicies"
     ]
     resources = ["arn:aws:eks:*:${data.aws_caller_identity.current.account_id}:*/*"]
     condition {
